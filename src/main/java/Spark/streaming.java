5e9f2edffdf4b4dd2874f34e2b66474bcddd8341
minhtien7749 <kocanbit19@gmail.com>
2022-08-20 21:21:59 +0700

    update

--- src/main/java/Spark/streaming.java
+++ src/main/java/Spark/streaming.java
@@ -12,8 +12,9 @@ public class streaming {
         SparkSession spark = SparkSession
                 .builder()
                 .appName("Spotify Streaming")
-                .master("local")
+//                .master("local")
                 .config("spark.dynamicAllocation.enabled","false")
+                .config("spark.master", "yarn")
                 .getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
         ReadStream(spark);
@@ -22,9 +23,9 @@ public class streaming {
     private static void ReadStream(SparkSession spark) throws TimeoutException, StreamingQueryException {
 
         StructType sc = new StructType().add("title", "STRING").
-                add("rank", "INT").
-                add("date", "TIMESTAMP").
-                add("artist", "FLOAT").
+                add("rank", "STRING").
+                add("date", "STRING").
+                add("artist", "STRING").
                 add("url", "STRING").
                 add("region", "STRING").
                 add("chart", "STRING").
@@ -35,9 +36,8 @@ public class streaming {
         Dataset<Row> df = spark
                 .readStream()
                 .format("kafka")
-                .option("kafka.bootstrap.servers", "172.17.80.26:9092")
+                .option("kafka.bootstrap.servers", "172.17.80.28:9092")
                 .option("subscribe", "spotify_tiencm8")
-                .option("group.id","group_id")
                 .option("startingOffsets","earliest")
                 .option("auto.offset.reset","true")
                 .option("failOnDataLoss", "false")
@@ -51,9 +51,10 @@ public class streaming {
         Dataset<Row> df4 = df2.select(df2.col("value.title"),
                 df2.col("value.rank"),
                 df2.col("value.date"),
+                df2.col("value.artist"),
                 df2.col("value.url"),
                 df2.col("value.region"),
-                df2.col("value.charts"),
+                df2.col("value.chart"),
                 df2.col("value.trend"),
                 df2.col("value.streams"));
 
@@ -63,10 +64,14 @@ public class streaming {
                 .writeStream()
                 .format("parquet")
                 .outputMode("append")
-                .option("checkpointLocation", "hdfs://172.17.80.21:9000/user/tiencm8/btl/checkpoint")
-                .option("path", "hdfs://172.17.80.21:9000/user/tiencm8/btl/output")
+                .option("checkpointLocation", "hdfs://172.17.80.21:9000/user/tiencm8/final/checkpoint")
+                .option("path", "hdfs://172.17.80.21:9000/user/tiencm8/final/output")
+//                E:\ETL_pipeline_spotify\data\output
+                //E:\ETL_pipeline_spotify\data\checkpoint
+                //hdfs://172.17.80.21:9000/user/tiencm8/final/checkpoint
+                //hdfs://172.17.80.21:9000/user/tiencm8/final/output
                 .start();
         query.awaitTermination();
-        spark.streams().awaitAnyTermination(2000);
+
     }
 }
--- src/main/java/kafka/producer1.py
+++ src/main/java/kafka/producer1.py
@@ -1,54 +1,61 @@
-
-import datetime
-
+import datetime as dt
+from datetime import datetime,timedelta,date
 import kafka
-# from airflow import DAG
-# from airflow.operators.dummy_operator import DummyOperator
-# from airflow.operators.python_operator import PythonOperator
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
 import csv
 from kafka import KafkaProducer,KafkaConsumer
 import os
-from datetime import date
+
 import pandas as pd
 import time
 import json
 
-# def my_producer():
-if __name__ == '__main__':
+def my_producer():
+    # if __name__ == '__main__':
     producer = KafkaProducer(
-    bootstrap_servers = ['localhost:9092'],
-    value_serializer=lambda x: json.dumps(x).encode('utf-8'))
-    
-    topic = 'spotify_tiencm8_final'
-    
+        bootstrap_servers = ['172.17.80.28:9092'],
+        value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
-    
-    delta = datetime.timedelta(days=-2055)
-    date1 =  date.today() + delta
+    topic = 'spotify_tiencm8_1'
+    file_r = open('/home/chibm/airflow/dags/count.txt', 'r')
+    checkpoin = int(file_r.read().strip()) + 1
+    file_r.close()
+    file_w = open('/home/chibm/airflow/dags/count.txt', 'w')
+    file_w.write(str(checkpoin))
+    file_w.close()
+
+
+
+    delta = dt.timedelta(days=-2057)
+    delta1 = dt.timedelta(days=checkpoin)
+
+    date1 =  date.today() + delta + delta1
     date_current = date1.strftime('%Y-%m-%d')
-    
-    #vòng lặp 1 năm
-    for i in range(1,365):
-        date2 = date1 +  datetime.timedelta(days=i)
-        date_current1 = date2.strftime('%Y-%m-%d')
-        df = pd.read_csv(f"E:\\data_spotify\\data\\{date_current1}.csv",encoding="utf-8")
-        df1 = df.fillna(0)
-        for row in df1.iterrows() :
-            print("sent",row[1].to_dict())
-            producer.send(topic, row[1].to_dict())
-            producer.flush()
-
-    
 
 
-# dag = DAG('csvToKafka', description='spotiy data to kafka',
-#           schedule_interval='@daily',
-#           start_date=datetime(2022, 8, 13), catchup=False)
-#
-# Kafka_Producer = PythonOperator(
-#     task_id='kafka_producer',
-#     python_callable=my_producer,
-#     dag=dag)
+    # for i in range(-2,365):
+    # date2 = date1 +  dt.timedelta(days=i)
+    # date_current1 = date2.strftime('%Y-%m-%d')
+    df = pd.read_csv(f"/home/tiencm8/data1/{date_current}.csv",encoding="utf-8")
+    df1 = df.fillna(0)
+    for row in df1.iterrows() :
+        producer.send(topic, row[1].to_dict())
+        producer.flush()
 
-#
-# Kafka_Producer
+
+
+
+dag = DAG('Kafka_spotify_test', description='spotiy data to kafka test',
+          schedule_interval='*/2 * * * *',
+          start_date=datetime(2022, 8, 20), catchup=False)
+
+Kafka_Producer = PythonOperator(
+    task_id='kafka_producer',
+    python_callable=my_producer,
+    dag=dag)
+
+
+Kafka_Producer
